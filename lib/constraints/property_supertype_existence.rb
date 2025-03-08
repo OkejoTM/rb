b25e78912cb59dcae8c725b34cfd88c2536a922e
Danila Kuzmin <denastytiko03@mail.ru>
2025-03-08 12:39:04 +0300

    f

--- lib/constraints/property_supertype_existence.rb
+++
@@ -1,7 +0,0 @@
-module Constraints
-  class PropertySupertypeExistence
-    def matches?(request)
-      PropertySupertype::SLUGS.values.include?(request.path_parameters[:property_supertype])
-    end
-  end
-end
--- lib/tasks/pictures.rake
+++
@@ -1,6 +0,0 @@
-namespace :pictures do
-  desc "Clear temporary pictures"
-  task clear_temporary: :environment do
-    Picture.temporary.destroy_all
-  end
-end
